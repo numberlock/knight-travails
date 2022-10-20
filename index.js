b5a83c3d07a12c86728ba9fa4b57c5c512ba6502
numberlock <numberlock@gmail.com>
2022-10-20 21:46:18 +0200

    create gameboard & knight

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 "use strict";
+const board = document.querySelector(".board");
+
+function createGrid() {
+  for (let i = 1; i <= 8; i++) {
+    const createRow = document.createElement("div");
+    for (let j = 8; j >= 1; j--) {
+      const createColumn = document.createElement("div");
+      createColumn.classList.add("square");
+      createColumn.dataset.cord = `${i},${j}`;
+      createColumn.textContent = `${i}`;
+      if (i % 2 === 0) {
+        if (j % 2 === 0) createColumn.classList.add("white-square");
+        else createColumn.classList.add("black-square");
+      } else {
+        if (j % 2 === 0) createColumn.classList.add("black-square");
+        else createColumn.classList.add("white-square");
+      }
+
+      createRow.appendChild(createColumn);
+    }
+    board.appendChild(createRow);
+  }
+}
+
+function createKnight() {
+  const createImg = document.createElement("img");
+  createImg.classList.add("knight");
+  createImg.dataset.cord = "1,1";
+  createImg.src = "knight.png";
+  const square = document.querySelector("[data-cord='1,1']");
+  square.appendChild(createImg);
+}
+
+createGrid();
+createKnight();
